515c70667c944f5ec48ee2c4969686fc1ed6a77c
Dario Griffo <dariogriffo@gmail.com>
2021-03-11 14:40:04 +0000

    1.1.0
    Remove comments at the end of the line and allow trim configurable behaviour.

--- src/EnvironmentFile/EnvironmentFileConfigurationExtensions.cs
+++ src/EnvironmentFile/EnvironmentFileConfigurationExtensions.cs
@@ -9,17 +9,43 @@ namespace Configuration.Extensions.EnvironmentFile
     {
         public static IConfigurationBuilder AddEnvironmentFile(
             this IConfigurationBuilder builder,
-            string fileName = ".env")
+            string fileName = ".env",
+            bool trim = true,
+            bool removeWrappingQuotes = true)
         {
             if (!File.Exists(fileName))
             {
                 return builder;
             }
 
-            var configuration = File
-                .ReadAllLines(fileName)
-                .Select(x => x.TrimStart())
-                .Where(x => !x.StartsWith("#") && x.Contains("="))
+            var nonCommentLinesWithPropertyValues =
+                File
+                    .ReadAllLines(fileName)
+                    .Select(x => x.TrimStart())
+                    .Where(x => !x.StartsWith("#") && x.Contains("="));
+
+            string ParseQuotes(string s)
+            {
+                if (!removeWrappingQuotes)
+                {
+                    return s;
+                }
+
+                var parts = s.Split('=');
+                s = string.Join("=", parts.Skip(1));
+                return $"{parts[0]}={s.Trim('"')}";
+            }
+
+            string RemoveCommentsAtTheEndAndTrimIfNecessary(string s1)
+            {
+                var value = s1.Split('#')[0];
+                return trim ? value.Trim() : value;
+            }
+
+            var configuration =
+                nonCommentLinesWithPropertyValues
+                .Select(ParseQuotes)
+                .Select(RemoveCommentsAtTheEndAndTrimIfNecessary)
                 .Select(x => x.Split('='))
                 .Select(x => new KeyValuePair<string, string>(x[0].Replace("__", ":"), string.Join("=", x.Skip(1))));
 
--- tests/EnvironmentFile.UnitTests/EnvironmentFileConfigurationExtensionsTests.cs
+++ tests/EnvironmentFile.UnitTests/EnvironmentFileConfigurationExtensionsTests.cs
@@ -54,5 +54,40 @@ namespace Configuration.Extensions.EnvironmentFile.UnitTests
             var configuration = new ConfigurationBuilder().AddEnvironmentFile(".env-with-equals-in-value").Build();
             configuration.GetConnectionString("Test").Should().Be("Value=1");
         }
+
+        [Fact]
+        public void AddEnvironmentFile_When_Configuration_Has_Comments_After_Variable_Value_File_Is_Correctly_Parsed()
+        {
+            var configuration = new ConfigurationBuilder().AddEnvironmentFile(".env-with-comments-at-end-of-line").Build();
+            configuration.GetConnectionString("WithComment").Should().Be("Value");
+        }
+
+        [Fact]
+        public void AddEnvironmentFile_When_Trim_Is_False_Is_Correctly_Parsed()
+        {
+            var configuration = new ConfigurationBuilder().AddEnvironmentFile(".env-no-trim", trim: false).Build();
+            configuration.GetConnectionString("Property").Should().Be("1 ");
+        }
+
+        [Fact]
+        public void AddEnvironmentFile_When_Trim_Is_True_Is_Correctly_Parsed()
+        {
+            var configuration = new ConfigurationBuilder().AddEnvironmentFile(".env-no-trim", trim: true).Build();
+            configuration.GetConnectionString("Property").Should().Be("1");
+        }
+
+        [Fact]
+        public void AddEnvironmentFile_RemoveWrappingQuotes_Is_False_Is_Correctly_Parsed()
+        {
+            var configuration = new ConfigurationBuilder().AddEnvironmentFile(".env-wrapping-quotes", removeWrappingQuotes: false).Build();
+            configuration.GetConnectionString("Property").Should().Be("\"1 \"");
+        }
+
+        [Fact]
+        public void AddEnvironmentFile_RemoveWrappingQuotes_Is_True_Is_Correctly_Parsed()
+        {
+            var configuration = new ConfigurationBuilder().AddEnvironmentFile(".env-wrapping-quotes", trim: false, removeWrappingQuotes: true).Build();
+            configuration.GetConnectionString("Property").Should().Be("1 ");
+        }
     }
 }
